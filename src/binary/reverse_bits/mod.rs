4760dfe789efaac547ec7b5a869b2bffd2d3c80a
gunb0s <bys20411@gmail.com>
2022-06-21 02:56:19 +0900

    reverse_bits: others

--- src/binary/reverse_bits/mod.rs
+++ src/binary/reverse_bits/mod.rs
@@ -2,27 +2,13 @@ use crate::binary::Binary;
 
 impl Binary {
     pub fn reverse_bits(x: u32) -> u32 {
-        let mut input_bit: [u32; 32] = [0; 32];
-        let mut x = x;
-        let mut idx = 0;
-        
-        while x > 0 {
-            if x % 2 == 1 {
-                input_bit[idx] = 1;
-            } else {
-                input_bit[idx] = 0;
-            }
-            x = x / 2;
-            idx += 1;
+        let (mut res, mut x) = (0u32, x);
+        for _ in 0..32 {
+            res = (res << 1) | (x & 1);
+            x >>= 1;
         }
 
-        let mut result = 0;
-
-        for i in (0..=31).rev() {
-            result += input_bit[31 - i] * u32::pow(2, i as u32);
-        }
-
-        result
+        res
     }
 }
 
